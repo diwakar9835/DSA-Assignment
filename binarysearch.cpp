c41bce43c8b03697aaa039c1a0013ce211f11da8
Ankit Diwakar <116998434+diwakar9835@users.noreply.github.com>
2025-04-20 21:44:27 +0530

    Update binarysearch.cpp

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -6,6 +6,7 @@ struct TreeNode {
     TreeNode *right;
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 };
+
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
