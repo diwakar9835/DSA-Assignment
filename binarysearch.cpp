f845e1bc1ba5326dffd467227250dc9d1b352ddb
Ankit Diwakar <116998434+diwakar9835@users.noreply.github.com>
2025-05-16 11:15:01 +0530

    Update binarysearch.cpp

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -4,8 +4,7 @@ struct TreeNode {
 int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode(int x) : val(x), 
-left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 };
 class Solution {
 
