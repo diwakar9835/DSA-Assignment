ee6ba4964cf588285cda3730168ba659157bcd5d
Ankit Diwakar <116998434+diwakar9835@users.noreply.github.com>
2025-07-04 23:14:16 +0530

    Update binarysearch.cpp

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -13,9 +13,6 @@ public:
             return new Tree
                 Node(val);
         }
-
-
-        
         
         if (val < root->val) {
             root->left = insertIntoBST(root->left, val);
