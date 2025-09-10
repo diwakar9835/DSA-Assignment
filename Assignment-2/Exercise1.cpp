6dab1406a8ac97691c6e6ac40232d5285566a734
Ankit Diwakar <116998434+diwakar9835@users.noreply.github.com>
2025-09-10 22:39:14 +0530

    Update Exercise1.cpp

--- Assignment-2/Exercise1.cpp
+++ Assignment-2/Exercise1.cpp
@@ -1,4 +1,3 @@
-
 // EXERCISE: 1
 // Given two arrays arr1[], arr2[], and an integer k. The task is to check if after permuting both arrays in such a way, we get the sum of their corresponding element greater than or equal to k i.e. arr1i + arr2i >= k for all i (from 0 to n-1). Return true if possible, else false.
 
@@ -33,4 +32,5 @@ int main() {
 
     return 0;
 }
+
 
